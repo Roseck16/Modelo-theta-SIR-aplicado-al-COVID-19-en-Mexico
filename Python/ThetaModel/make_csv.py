639ba85a0304b79b99972de810557b58e852f0e1
YolticCruzTello <ticyolollin@hotmail.com>
2021-03-16 01:41:07 -0600

    Changed dictionary to be a list of dictionaries

--- Python/ThetaModel/make_csv.py
+++ Python/ThetaModel/make_csv.py
@@ -1,5 +1,7 @@
 import pandas as pd
+import numpy as np
 import math
+import csv
 from scipy.integrate import odeint
 
 path = "Datos/210103COVID19MEXICO.csv"
@@ -11,7 +13,7 @@ data = pd.read_csv(path, encoding=enc, engine="c", chunksize=20)
 c = data.get_chunk()
 c['FECHA_INGRESO']
 
-days = {}
+days = []
 
 for index, row in c.iterrows():
 
@@ -23,18 +25,37 @@ for index, row in c.iterrows():
 
     if fecha not in days:
         days[fecha] = {"Hos" : 0, "Imp" : 0, "Pos" : 0, "Pos_u" : 0, "Fall" : 0}
-        if hos == 2 and clas == 3:
-            days[fecha]["Hos"] += 1
-        if 
-    
+
+    if hos == 2 and clas == 3:
+        days[fecha]["Hos"] += 1
+    if imp == 2 and clas == 3:
+        days[fecha]["Imp"] += 1
+    if clas == 3:
+        days[fecha]["Pos"] += 1
+    if clas in (1,2,4,5,6):
+        days[fecha]["Pos_u"] += 1
+    if fall != '9999-99-99':
+        days[fecha]["Fall"] += 1
+
 
 # hospitalizados, importados, positivos, positivos no detectados, fallecidos
-days = {
-    "2020-04-02" : {"Hos" : 0, "Imp" : 0, "Pos" : 1, "Pos_u" : 0, "Fall" : 0},
-    "2020-03-26" : {"Hos" : 1, "Imp" : 0, "Pos" : 1, "Pos_u" : 0, "Fall" : 1},
-    "2020-03-28" : {"Hos" : 1, "Imp" : 0, "Pos" : 1, "Pos_u" : 0, "Fall" : 1},
-    "2020-03-31" : {"Hos" : 0, "Imp" : 0, "Pos" : 1, "Pos_u" : 0, "Fall" : 0}
-}
+days = [
+    {'Fecha' : "2020-04-02", "Hos" : 0, "Imp" : 0, "Pos" : 1, "Pos_u" : 0, "Fall" : 0},
+    {'Fecha' : "2020-03-26", "Hos" : 1, "Imp" : 0, "Pos" : 1, "Pos_u" : 0, "Fall" : 1},
+    {'Fecha' : "2020-03-28", "Hos" : 1, "Imp" : 0, "Pos" : 1, "Pos_u" : 0, "Fall" : 1},
+    {'Fecha' : "2020-03-31", "Hos" : 0, "Imp" : 0, "Pos" : 1, "Pos_u" : 0, "Fall" : 0}
+]
 
-if True:
-    days["2020-03-31"]["Hos"] += 1
+for dics in days:
+    if dics['Fecha'] == "2020-03-31":
+        print(dics)
+
+a = np.zeros(400, dtype=object)
+a[0] = {'Fecha' : "2020-04-02", "Hos" : 0, "Imp" : 0, "Pos" : 1, "Pos_u" : 0, "Fall" : 0}
+np.trim_zeros(a)
+
+def revisar_fecha(lista_dics, fecha):
+    for dic in lista_dics:
+        if dic['Fecha'] == fecha:
+            return False
+    return True
