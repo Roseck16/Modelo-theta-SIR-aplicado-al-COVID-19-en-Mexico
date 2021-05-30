a65b78c67729e3ee673b2aa821ee20ceb9d0b47a
YolticCruzTello <ticyolollin@hotmail.com>
2021-05-30 02:53:49 -0500

    Updated files

--- Julia/Theta model/ThetaModel.jl
+++ Julia/Theta model/ThetaModel.jl
@@ -5,25 +5,29 @@ function ThetaModel!(du::M, u::M, p::M, t::Float64) where {M<:Vector{Float64}}
     S, E, I, Iu, hr, hd, Q, _, _, _ = u
     γ_d, γ_E, γ_I, γ_Iu, γ_Hr, γ_Hd, γ_Q, k2, c3, c5, ω_0, ω_CFR0, θ_0, ω, β_I0, c_E, c_u, ρ0, ω_u = p
 
+    delays = rounder!(γ_d, γ_E+γ_I)
+    # TODO: calculate new time-parameters only when the rounded value of *t* has changed
+    # TODO: calculate new βs only when *t* hits one of the values in *dates*
+
+
     #t >= 2.0 ? error("Its time") : println("Hello")
-    ms = Msλs(dates, [(k2)], [(c3), (c5)])
-    ω, θ, η, ρ, τ1, τ2 = TimeParams(
-        trunc(Int64, t), data,
-        t0, tMAX, t_iCFR, t_θ0, t_η,
-        trunc(Int64,γ_d), trunc(Int64,γ_E), trunc(Int64,γ_I),
-        ρ0,
-        ω_0, ω_CFR0, θ_0, _ω=ω
-        )
-    
-    β_e, β_I, β_Iu, β_hr, β_hd = βs(
-        trunc(Int64, t),
-        ω, θ, ω_u, η, ρ,
-        ms, Float64.(λs),
-        trunc(Int64,γ_E), trunc(Int64,γ_I), trunc(Int64,γ_Iu), trunc(Int64,γ_Hr), trunc(Int64,γ_Hd), 
-        β_I0, c_E, c_u
-        )
-        # b1 = β_e * E + β_I * I + β_Iu * Iu + β_IDu * IDu + β_hr * hr + β_hd * hd
-    
+    # ms = Msλs(dates, [(k2)], [(c3), (c5)])
+    # ω, θ, η, ρ, τ1, τ2 = TimeParams(
+    #     trunc(Int64, t), data,
+    #     t0, tMAX, t_iCFR, t_θ0, t_η,
+    #     trunc(Int64,γ_d), trunc(Int64,γ_E), trunc(Int64,γ_I),
+    #     ρ0,
+    #     ω_0, ω_CFR0, θ_0, _ω=ω
+    #     )
+
+    # β_e, β_I, β_Iu, β_hr, β_hd = βs(
+    #     trunc(Int64, t),
+    #     ω, θ, ω_u, η, ρ,
+    #     ms, Float64.(λs),
+    #     trunc(Int64,γ_E), trunc(Int64,γ_I), trunc(Int64,γ_Iu), trunc(Int64,γ_Hr), trunc(Int64,γ_Hd),
+    #     β_I0, c_E, c_u
+    #     )
+
     # ODE system
     # du[1] = -(S/N) * b1
     # du[2] = (S/N) * b1 - E/γ_E + τ1 - τ2
@@ -40,45 +44,46 @@ function ThetaModel!(du::M, u::M, p::M, t::Float64) where {M<:Vector{Float64}}
     # du[10] = hd/γ_Hd
 end
 
-function ThetaModel!(du, u, p, t)
-    S, E, I, Iu, hr, hd, Q, _, _, _ = u
-    γ_d, γ_E, γ_I, γ_Iu, γ_Hr, γ_Hd, γ_Q, k2, c3, c5, ω_0, ω_CFR0, θ_0, ω, β_I0, c_E, c_u, ρ0, ω_u = p
+# TODO: make the model for when the input parameters are ReverseDiff.TrackedReal()
+# function ThetaModel!(du, u, p, t)
+#     S, E, I, Iu, hr, hd, Q, _, _, _ = u
+#     γ_d, γ_E, γ_I, γ_Iu, γ_Hr, γ_Hd, γ_Q, k2, c3, c5, ω_0, ω_CFR0, θ_0, ω, β_I0, c_E, c_u, ρ0, ω_u = p
 
-    t >= 1.5 ? error("Its time") : display(typeof(p))
-    ms, λs = Msλs(dates, [ReverseDiff.value(k2)], [ReverseDiff.value(c3), ReverseDiff.value(c5)])
-    
-    ω, θ, η, ρ, τ1, τ2 = TimeParams(
-        ReverseDiff.value(t), data,
-        ReverseDiff.value(t0), ReverseDiff.value(tMAX), ReverseDiff.value(t_iCFR), ReverseDiff.value(t_θ0), ReverseDiff.value(t_η),
-        ReverseDiff.value(γ_d), ReverseDiff.value(γ_E), ReverseDiff.value(γ_I),
-        ReverseDiff.value(ρ0),
-        ReverseDiff.value(ω_0), ReverseDiff.value(ω_CFR0), ReverseDiff.value(θ_0), _ω=ReverseDiff.value(ω)
-        )
-    
-    β_e, β_I, β_Iu, β_hr, β_hd = βs(
-        ReverseDiff.value(t),
-        ReverseDiff.value(ω), ReverseDiff.value(θ), ReverseDiff.value(ω_u), ReverseDiff.value(η), ReverseDiff.value(ρ),
-        ReverseDiff.value(ms), ReverseDiff.value(λs),
-        ReverseDiff.value(γ_E), ReverseDiff.value(γ_I), ReverseDiff.value(γ_Iu), ReverseDiff.value(γ_Hr), ReverseDiff.value(γ_Hd), 
-        ReverseDiff.value(β_I0), ReverseDiff.value(c_E), ReverseDiff.value(c_u)
-        )
-        # b1 = β_e * E + β_I * I + β_Iu * Iu + β_IDu * IDu + β_hr * hr + β_hd * hd
-    
-    # ODE system
-    # du[1] = -(S/N) * b1
-    # du[2] = (S/N) * b1 - E/γ_E + τ1 - τ2
-    du[1] = -(S / N::Float64) * (β_e * E + β_I * I + β_Iu * Iu + β_hr * hr + β_hd * hd)
-    du[2] = (S / N::Float64) * (β_e * E + β_I * I + β_Iu * Iu + β_hr * hr + β_hd * hd) - E/γ_E + τ1 - τ2
-    du[3] = E/γ_E - I/γ_I
-    du[4] = (1 - θ - ω_u) * I/γ_I - Iu/γ_Iu
-    # du[5] = ω_u * I/γ_I
-    du[5] = ρ * (θ - ω) * I/γ_I - hr/γ_Hr
-    du[6] = ω * I/γ_I - hd/γ_Hd
-    # du[7] = (1 - ρ) * (θ - ω) * I/γ_I + hr/γ_Hr - γ_Q * Q
-    # du[8] = Q/γ_Q
-    # du[9] = Iu/γ_Iu
-    # du[10] = hd/γ_Hd
-end
+#     t >= 1.5 ? error("Its time") : display(typeof(p))
+#     ms, λs = Msλs(dates, [ReverseDiff.value(k2)], [ReverseDiff.value(c3), ReverseDiff.value(c5)])
+
+#     ω, θ, η, ρ, τ1, τ2 = TimeParams(
+#         ReverseDiff.value(t), data,
+#         ReverseDiff.value(t0), ReverseDiff.value(tMAX), ReverseDiff.value(t_iCFR), ReverseDiff.value(t_θ0), ReverseDiff.value(t_η),
+#         ReverseDiff.value(γ_d), ReverseDiff.value(γ_E), ReverseDiff.value(γ_I),
+#         ReverseDiff.value(ρ0),
+#         ReverseDiff.value(ω_0), ReverseDiff.value(ω_CFR0), ReverseDiff.value(θ_0), _ω=ReverseDiff.value(ω)
+#         )
+
+#     β_e, β_I, β_Iu, β_hr, β_hd = βs(
+#         ReverseDiff.value(t),
+#         ReverseDiff.value(ω), ReverseDiff.value(θ), ReverseDiff.value(ω_u), ReverseDiff.value(η), ReverseDiff.value(ρ),
+#         ReverseDiff.value(ms), ReverseDiff.value(λs),
+#         ReverseDiff.value(γ_E), ReverseDiff.value(γ_I), ReverseDiff.value(γ_Iu), ReverseDiff.value(γ_Hr), ReverseDiff.value(γ_Hd),
+#         ReverseDiff.value(β_I0), ReverseDiff.value(c_E), ReverseDiff.value(c_u)
+#         )
+#         # b1 = β_e * E + β_I * I + β_Iu * Iu + β_IDu * IDu + β_hr * hr + β_hd * hd
+
+#     # ODE system
+#     # du[1] = -(S/N) * b1
+#     # du[2] = (S/N) * b1 - E/γ_E + τ1 - τ2
+#     du[1] = -(S / N::Float64) * (β_e * E + β_I * I + β_Iu * Iu + β_hr * hr + β_hd * hd)
+#     du[2] = (S / N::Float64) * (β_e * E + β_I * I + β_Iu * Iu + β_hr * hr + β_hd * hd) - E/γ_E + τ1 - τ2
+#     du[3] = E/γ_E - I/γ_I
+#     du[4] = (1 - θ - ω_u) * I/γ_I - Iu/γ_Iu
+#     # du[5] = ω_u * I/γ_I
+#     du[5] = ρ * (θ - ω) * I/γ_I - hr/γ_Hr
+#     du[6] = ω * I/γ_I - hd/γ_Hd
+#     # du[7] = (1 - ρ) * (θ - ω) * I/γ_I + hr/γ_Hr - γ_Q * Q
+#     # du[8] = Q/γ_Q
+#     # du[9] = Iu/γ_Iu
+#     # du[10] = hd/γ_Hd
+# end
 
 """
     check(dt,u,p,t)
@@ -94,15 +99,16 @@ function check(dt, u, p, t)
     return false
 end
 
+# TODO: update this function according to the model function
 function distance(x)
-    
+
     # Assing the values in ´x´ to a variable
     γ_E, γ_I, γ_Iu, γ_Hr, γ_Hd, γ_Q, β_I0, c_E, c_u, ρ0, k2, c3, c5, ω_u0 = x
-    
+
     # Get some of the parameters that are known
     ω = get(saved, "ω", 0.0)::Float64
     γ_d = trunc(Int64,get(saved, "γ_d", 0))
-    
+
     # These are non time-dependent parameters that we can already calculate or retrieve from a Dictionary
     ω_0 = get(saved, "ω_0") do
         get_ω(t_θ0, ms, λs, max_ω, min_ω)
@@ -113,7 +119,7 @@ function distance(x)
     θ_0 = get(saved, "θ0") do
         get_θ(t_θ0, t_θ0, ω_0, ω, ω_CFR0, 0.0)
     end
-    
+
     p = [
         γ_d, γ_E, γ_I, γ_Iu, γ_Hr, γ_Hd, γ_Q, # gammas
         k2, c3, c5,
@@ -121,10 +127,16 @@ function distance(x)
         β_I0, c_E, c_u, ρ0, ω_u0
     ]
     # Solve the ODE problem and calculate the difference between the solution and the real data
-    
+
     sol = solve(prob, p=p, tstops=tsteps, unstable_check=check)
-    
+
     days = map(x -> trunc(x), sol.t)
     distance = sqrt(sum(abs2, sol[3,:] .- data.infec[days]))
     return distance, sol
+end
+
+function graf_predictions(data::Data, solution, labels::Matrix{String})
+    xs = map(x -> round(Int, x),solution.t)
+    ys = [solution[3,:], data.infec[xs]]
+    plot(xs,ys, label=labels)
 end
--- Julia/Theta model/main.jl
+++ Julia/Theta model/main.jl
@@ -4,45 +4,32 @@ using Plots, Flux, Optim, DiffEqFlux, DiffEqSensitivity, ReverseDiff
 using BenchmarkTools
 #using BSON: @save, @load
 
-function graf_predictions(data::Data, solution, labels::Matrix{String})
-    xs = map(x -> round(Int, x),solution.t)
-    ys = [solution[3,:], data.infec[xs]]
-    plot(xs,ys, label=labels)
-end
+# const saved = Dict{String,Float64}(
+#     "γ_d" => 13.123753454738198,
+#     #"γ_E" => 1/5.5,
+#     #"γ_I" => 1/5,
+#     "ω" => 0.014555,
+#     "ω_0" =>0.50655
+# )
 
-const t0, tMAX, t_iCFR, t_θ0, t_η = 0, 446, 23, 29, 13
-const saved = Dict{String,Float64}(
-    "γ_d" => 13.123753454738198,
-    #"γ_E" => 1/5.5,
-    #"γ_I" => 1/5,
-    "m0" => 1.0,
-    "m1" => 1.0,
-    "m4" => 0.0,
-    "ω" => 0.014555,
-    "ω_0" =>0.50655
-)
+# * Get the data
+path = "D:\\Code\\[Servicio Social]\\Datos\\Casos_Modelo_Theta_final_complemented.csv"
+const data = Data(path)
 
-const lambdas = [
+lambdas = [
     "2020-03-20",
     "2020-03-23",
     "2020-03-30",
     "2020-04-21",
     "2020-06-01"
 ]
+dates = day_to_index(lambdas, data, Float64)
 
-labels = ["Model" "Real data"]
-
-# Get the data
-path = "D:\\Code\\[Servicio Social]\\Datos\\Casos_Modelo_Theta_final_complemented.csv"
-const data = Data(path)
-
-const dates = day_to_index(lambdas, data)
-
-# Time interval and intermediary points
+# * Time interval and intermediary points
 tspan = (1.0, 446.0)
 tsteps = 1.0:1.0:446.0
 
-# Inityal conditions
+# * Initial conditions
 N = Float64(data.population[Int(tspan[1])])
 u0 = [
     data.susceptible[Int(tspan[1])],
@@ -59,38 +46,65 @@ u0 = [
     data.dead[Int(tspan[1])]
 ]
 
-# θ-M equation parameters. 
+# * θ-M equation parameters. 
+
+# t0, tMAX, t_iCFR, t_θ0, t_η
+const times = [0, 446, 23, 29, 13]
+
 γ_E, γ_I, γ_Iu, γ_Hr, γ_Hd, γ_Q = 5.5, 5.0, 9.0, 14.2729, 5.0, 36.0450
+γ_d = 13.123753454738198
+delays = rounder!(γ_d, γ_E+γ_I)
+
 β_I0, c_E, c_u, ρ0, k2, c3, c5, ω_u0 =  0.4992, 0.3806, 0.3293, 0.7382, 1.0, 1.0, 1.0, 0.42
 β_e0, β_I0_min = (c_E, c_u) .* β_I0
-γ_d = trunc(Int64,get(saved, "γ_d", 0))
-ω = get(saved, "ω", 0.0)
 
-ms = Msλs(dates, [k2], [c3, c5])
+ω = 0.014555
+ω_0 = 0.50655
+ω_CFR0 = 147.71428571428572
 
-ω_0 = get(saved, "ω_0") do
-    get_ω(t_θ0, ms, λs, max_ω, min_ω)
-end
-ω_CFR0 = get(saved, "ω_CFR0") do
-    get_ω_CFR(Int(t_θ0), data, Int(t_iCFR), γ_d)
-end
-θ_0 = get(saved, "θ0") do
-    ω_0 / ω_CFR0
-end
+# ω_0 = get(saved, "ω_0") do
+#     get_ω(t_θ0, ms, λs, max_ω, min_ω)
+# end
+# ω_CFR0 = get(saved, "ω_CFR0") do
+#     get_ω_CFR(t_θ0, data, t_iCFR, delays[1])
+# end
+θ_0 = ω_0 / ω_CFR0
 
-ω_CFR = get_ω_CFR(1, data, t_iCFR, γ_d)
+ω_CFR = get_ω_CFR(1, data, t_iCFR, delays[1])
+
+ms = Msλs(convert(Float64,t0), λs, [k2], [c3, c5])
+
 θ = 1 <= t_θ0 ? ω_0 / ω_CFR0 : ω / ω_CFR
 ρ = get_ρ(ω_0, ω, θ_0, θ, ρ0)
-η = get_η(1, data, t0, tMAX, t_η, γ_E, γ_I)
+η = get_η(1, data, t0, tMAX, t_η, delays[2])
+
+# * Test if the parameter functions are working and their running time
+
+@benchmark Msλs(convert(Float64,t0), λs, [k2], [c3, c5])
+
+@benchmark TimeParams(
+    1, data, 
+    times, 
+    delays,
+    ρ0, ω_0, ω_CFR0, θ_0, _ω=ω
+)
+
+@benchmark βs(
+    1, 
+    ω, θ, ω_u0, η, ρ, 
+    ms, dates,
+    γ_E, γ_I, γ_Iu, γ_Hr, γ_Hd, 
+    β_I0, β_e0, β_I0_min
+)
 
 p = [
-    γ_d, γ_E, γ_I, γ_Iu, γ_Hr, γ_Hd, γ_Q, # gammas
+    γ_d, γ_E, γ_I, γ_Iu, γ_Hr, γ_Hd, γ_Q,
     k2, c3, c5,
     ω_0, ω_CFR0, θ_0, ω,
     β_I0, c_E, c_u, ρ0, ω_u0
 ]
 
-# Set up the ODE problem
+# * Set up the ODE problem and solve
 prob = ODEProblem(ThetaModel!, u0, tspan, p)
 
 # AutoVern7(Rodas5())
@@ -178,7 +192,9 @@ for i in 100.0:200.0
     end
 end
 
-# Plot the solution
+# * Plot the solution
+labels = ["Model" "Real data"]
+
 graf_predictions(data, sol, labels)
 
 callback = function(p, l, pred)
